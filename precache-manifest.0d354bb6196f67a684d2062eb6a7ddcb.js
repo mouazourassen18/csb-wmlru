0a1bf346ac11ee997aa4971d56ddac9100d15e13
Code Sandbox <hello@codesandbox.io>
2021-08-25 02:16:54 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0d354bb6196f67a684d2062eb6a7ddcb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533ae30cf628cd7dea6ff09f72fd12f4",
+    "url": "https://mouazourassen18.github.io/csb-wmlru/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "https://mouazourassen18.github.io/csb-wmlru/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "9eb76ddee40daac53df4",
+    "url": "https://mouazourassen18.github.io/csb-wmlru/static/js/main.18cdbbe6.chunk.js"
+  },
+  {
+    "revision": "954b40d3fe671c7c6d60",
+    "url": "https://mouazourassen18.github.io/csb-wmlru/static/js/runtime-main.0d13e9f5.js"
+  }
+]);
